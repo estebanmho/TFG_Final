031be482951203340bf22605083e40ae5439fa89
estebanmho <esteban.martinez.hoces@alumnos.upm.es>
2023-01-06 11:44:57 +0100

    Exceptions in UI and update paths

--- UI/UI.py
+++ UI/UI.py
@@ -3,35 +3,58 @@ import tkinter
 from Depth.calibration_images import *
 from Depth.stereo_calibration import *
 from Prediction.PredictiveClass import *
+import traceback
+import logging
 
 camera_one_value = 0
 
 def hide_camera_two():
-    if checked_state.get() == 1:
-        camera_two.grid(row=2, column=1, padx=10, pady=10)
-        camera_two_label.grid(row=2, column=0, padx=10, pady=10)
-    else:
-        camera_two.grid_remove()
-        camera_two_label.grid_remove()
+    try:
+        if checked_state.get() == 1:
+            camera_two.grid(row=2, column=1, padx=10, pady=10)
+            camera_two_label.grid(row=2, column=0, padx=10, pady=10)
+        else:
+            camera_two.grid_remove()
+            camera_two_label.grid_remove()
+    except Exception as e:
+        tkinter.messagebox.showinfo("Log", traceback.format_exc())
+        window.deiconify()
+
 
 
 def start_program():
     window.withdraw()
+    tkinter.messagebox.showinfo("Instructions", "Press q to stop tracking.")
     execution = PredictiveClass(int(camera_one.get()))
     if checked_state.get() == 1:
         execution.include_depth(int(camera_two.get()))
-    execution.main_function()
-    window.destroy()
+    try:
+        execution.main_function()
+        window.destroy()
+    except Exception as e:
+        tkinter.messagebox.showinfo("Instructions", "Camara not detected or not compatible.")
+        tkinter.messagebox.showinfo("Log", traceback.format_exc())
+        window.deiconify()
 
 
 def calibrate():
-    window.withdraw()
-    var_cal = CalibrationImages(int(camera_one.get()), int(camera_two.get()))
-    var_cal.start_calibration()
-    stereo_calibrate()
-    tkinter.messagebox.showinfo("Instructions", "The cameras has been calibrated")
+    try:
+        window.withdraw()
+        if checked_state.get() == 1:
+            if int(camera_one.get()) == int(camera_two.get()):
+                tkinter.messagebox.showinfo("Instructions", "Same camera selected.")
+            else:
+                var_cal = CalibrationImages(int(camera_one.get()), int(camera_two.get()))
+                var_cal.start_calibration()
+                stereo_calibrate()
+                tkinter.messagebox.showinfo("Instructions", "The cameras has been calibrated.")
+        else:
+            tkinter.messagebox.showinfo("Instructions", "No second camera configurated.")
+        window.deiconify()
+    except Exception as e:
+        tkinter.messagebox.showinfo("Log", traceback.format_exc())
+        window.deiconify()
 
-    window.deiconify()
 
 
 window = tkinter.Tk()
